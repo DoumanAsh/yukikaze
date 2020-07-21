df08834a355a6c0f8fa52dc7a37a128521d9edfd
Douman <douman@gmx.se>
2020-07-21 23:04:07 +0200

    1.0.8: Use rand for random too

--- src/upgrade/websocket.rs
+++ src/upgrade/websocket.rs
@@ -1,6 +1,5 @@
 //! Websocket upgrade module
 
-use core::mem;
 use core::ops::Deref;
 use std::error::Error;
 
@@ -90,8 +89,10 @@ impl super::Upgrade for WebsocketUpgrade {
     type Options = Option<WebsocketUpgradeOpts>;
 
     fn prepare_request(headers: &mut http::HeaderMap, extensions: &mut http::Extensions, options: Self::Options) {
-        let mut sec_key: [u8; 16] = unsafe { mem::MaybeUninit::uninit().assume_init() };
-        let _ = getrandom::getrandom(&mut sec_key);
+        use ring::rand::SecureRandom;
+
+        let mut sec_key: [u8; 16] = [2, 3, 99, 255, 243, 125, 17, 29, 93, 105, 201, 152, 145, 192, 200, 221];
+        let _ = ring::rand::SystemRandom::new().fill(&mut sec_key);
 
         let encode_len = BASE64.encode_len(sec_key.len());
         let mut key = bytes::BytesMut::with_capacity(encode_len);
